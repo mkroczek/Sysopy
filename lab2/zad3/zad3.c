fc47e7bed81a6ccdd9370adcb10c3b6413b12886
mkroczek <1marcin100@gmail.com>
2021-03-17 08:26:52 +0100

    Folders for each lab created

---
+++ lab2/zad3/zad3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    FILE* data = fopen("dane.txt", "r");
+    FILE* a = fopen("a.txt","w");
+    FILE* b = fopen("b.txt", "w");
+
+    return 0;
+}
